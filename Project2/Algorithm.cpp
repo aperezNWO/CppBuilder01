45b0457e1eac803fc4737383db913567389f8887
alejandro.perez.acosta@hotmail.com <aperezNWO>
2023-11-07 16:02:58 -0500

    comenarios

--- Project2/Algorithm.cpp
+++ Project2/Algorithm.cpp
@@ -23,4 +23,10 @@
 			std::unique_ptr<Dijkstra> uniquePtr = std::make_unique<Dijkstra>();
 			return uniquePtr->GetRandomPoints(p_vertexSize, p_sourcePoint).c_str();
 		};
+
+		/*
+
+  		"http://localhost:83/demos/_GetSort?p_sortAlgoritm=1&p_unsortedList=13|3|9|10|19|16|0|2|18|20|24|23|7|5|12|1|21|4|22|15|6|8|14|11|17" succeeded in 143 ms.
+
+		*/
 	}
