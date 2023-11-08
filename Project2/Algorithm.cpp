201496c22cdc224dd01095ab10300bb79bcb2de0
alejandro.perez.acosta@hotmail.com <aperezNWO>
2023-11-08 03:33:03 -0500

    SortBenchmark -> call functions "BubbleSort()" and "QuickSort()"

--- Project2/Algorithm.cpp
+++ Project2/Algorithm.cpp
@@ -30,7 +30,12 @@
 		DLL_EXPORT const char* SortBenchMark_GetSort(int p_sortAlgoritm, char* p_unsortedList)
 		{
 			//
-			std::unique_ptr<SortBenchMark> uniquePtr = std::make_unique<SortBenchMark>();
+			int ARRAY_SIZE      = 25;
+			//
+			std::unique_ptr<SortBenchMark> uniquePtr = std::make_unique<SortBenchMark>(
+					  p_unsortedList
+					, ARRAY_SIZE
+			);
 			return uniquePtr->GetSort(p_sortAlgoritm, p_unsortedList).c_str();
 		};
 
