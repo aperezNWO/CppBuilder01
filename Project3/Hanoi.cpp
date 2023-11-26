5cd0dd38ea85298957f588baa273d9fb9e1efaec
alejandro.perez.acosta@hotmail.com <aperezNWO>
2023-11-26 09:34:24 -0500

    Sudoku

--- Project3/Hanoi.cpp
+++ Project3/Hanoi.cpp
@@ -1,6 +1,8 @@
 //https://www.geeksforgeeks.org/c-program-for-tower-of-hanoi/
 // C++ recursive function to
 // solve tower of hanoi puzzle
+// This is code is contributed by rathbhupendra
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
@@ -8,34 +10,36 @@
 
 using namespace std;
 
-void towerOfHanoi(int n, char from_rod, char to_rod,
-				char aux_rod)
+class Hanoi
 {
-	if (n == 0) {
-		return;
+    //
+	void towerOfHanoi(int n, char from_rod, char to_rod,
+					char aux_rod)
+	{
+		if (n == 0) {
+			return;
+		}
+		towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+		cout << "Move disk " << n << " from rod " << from_rod
+			<< " to rod " << to_rod << endl;
+		towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
 	}
-	towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
-	cout << "Move disk " << n << " from rod " << from_rod
-		<< " to rod " << to_rod << endl;
-	towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
-}
 
-// Driver code
-/*
-int main()
-{
-	int N = 3;
+	// Driver code
+	int Run()
+	{
+		int N = 3;
 
-	// A, B and C are names of rods
-	towerOfHanoi(N, 'A', 'C', 'B');
+		// A, B and C are names of rods
+		towerOfHanoi(N, 'A', 'C', 'B');
 
-	// Wait for the user to press Enter
-	std::cout << "Press Enter to continue...";
-	std::cin.get();
+		// Wait for the user to press Enter
+		std::cout << "Press Enter to continue...";
+		std::cin.get();
 
-	return 0;
-}
-*/
+		return 0;
+	};
+};
 
-// This is code is contributed by rathbhupendra
+
 
--- Project3/UnitTesting.cpp
+++ Project3/UnitTesting.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <tchar.h>
+#include <stdio.h>
+#include <Sudoku.cpp>
 
+//
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//
+	int N                   = 9;
+	int K                   = 20;
+	SudokuGenerator* sudoku = new SudokuGenerator(N, K);
+	sudoku->Run();
+
+	// Wait for the user to press Enter
+	std::cout << "Press Enter to continue...";
+	std::cin.get();
+
+	SudokuSolver* sudokuSolver = new SudokuSolver();
+	sudokuSolver->Run();
+
+	// Wait for the user to press Enter
+	std::cout << "Press Enter to continue...";
+	std::cin.get();
+
+	//
+	return (0);
+};
